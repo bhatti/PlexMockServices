9515d65d9dacc79d7f8eed6ee2d42c7ca8d38782
Shahzad Bhatti <sbhatti@sbhatti-macbook.local>
2017-03-16 16:05:53 -0700

    Added gretty

---
+++ run.sh
@@ -0,0 +1,3 @@
+export MAVEN_OPTS=-Xmx512m 
+#./gradlew war jettyRun
+./gradlew appRun
--- src/main/java/com/plexobject/mock/server/Server.java
+++ src/main/java/com/plexobject/mock/server/Server.java
@@ -9,6 +9,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,13 +32,7 @@ import com.plexobject.mock.util.FileUtils;
 public class Server extends HttpServlet {
 	private static final Logger LOGGER = Logger.getLogger(Server.class);
 	private static final long serialVersionUID = 1L;
-	private static final int CONNECTION_TIMEOUT_MILLIS = Integer
-			.parseInt(System.getProperty("connection.timeout.millis", "60000"));
-
 	private final HttpClient httpClient;
-	private static final boolean IS_RECORD_MODE = System.getProperty(
-			"MOCK_MODE", "record").equalsIgnoreCase("record");
-	private static String URL_PREFIX = System.getProperty("MOCK_URL_PREFIX");
 
 	static class Response implements Serializable {
 		private static final long serialVersionUID = 1L;
@@ -46,8 +41,7 @@ public class Server extends HttpServlet {
 		Map<String, String[]> headers;
 		String contentType;
 
-		public Response(int responseCode, byte[] payload, String contentType,
-				Map<String, String[]> headers) {
+		public Response(int responseCode, byte[] payload, String contentType, Map<String, String[]> headers) {
 			this.responseCode = responseCode;
 			this.payload = payload;
 			this.contentType = contentType;
@@ -88,43 +82,45 @@ public class Server extends HttpServlet {
 
 	}
 
+	private int connectionTimeoutMillis;
+	private boolean recordMode;
+	private String urlPrefix;
 	public Server() {
 		MultiThreadedHttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
 		httpClient = new HttpClient(connectionManager);
-
 		httpClient.getParams().setCookiePolicy(CookiePolicy.IGNORE_COOKIES);
-		httpClient.getParams().setSoTimeout(CONNECTION_TIMEOUT_MILLIS);
-		httpClient.getParams().setParameter("http.socket.timeout",
-				new Integer(CONNECTION_TIMEOUT_MILLIS));
-		httpClient.getParams().setParameter("http.connection.timeout",
-				new Integer(CONNECTION_TIMEOUT_MILLIS));
-		if (IS_RECORD_MODE && URL_PREFIX == null || URL_PREFIX.length() == 0) {
-			throw new RuntimeException(
-					"Running in record mode but mock.target.url property is not set");
+	}
+	
+	public void init(ServletConfig servletConfig) throws ServletException {
+		super.init(servletConfig);
+		connectionTimeoutMillis = Integer.parseInt(servletConfig.getInitParameter("connectionTimeoutMillis"));
+		recordMode = "true".equals(servletConfig.getInitParameter("recordMode"));
+		urlPrefix = servletConfig.getInitParameter("urlPrefix");
+
+		httpClient.getParams().setSoTimeout(connectionTimeoutMillis);
+		httpClient.getParams().setParameter("http.socket.timeout", connectionTimeoutMillis);
+		httpClient.getParams().setParameter("http.connection.timeout", connectionTimeoutMillis);
+
+		if (recordMode && (urlPrefix == null || urlPrefix.length() == 0)) {
+			throw new RuntimeException("Running in record mode but mock.target.url property is not set");
 		}
-		if (!URL_PREFIX.startsWith("http://")
-				&& !URL_PREFIX.startsWith("https://")) {
-			throw new RuntimeException("Invalid mock.target.url property '"
-					+ URL_PREFIX + "'");
+		if (!urlPrefix.startsWith("http://") && !urlPrefix.startsWith("https://")) {
+			throw new RuntimeException("Invalid mock.target.url property '" + urlPrefix + "'");
 		}
-		if (URL_PREFIX.endsWith("/")) {
-			URL_PREFIX = URL_PREFIX.substring(0, URL_PREFIX.length() - 1);
+		if (urlPrefix.endsWith("/")) {
+			urlPrefix = urlPrefix.substring(0, urlPrefix.length() - 1);
 		}
-		LOGGER.info("TARGET URL PREFIX " + URL_PREFIX + " record mode ? "
-				+ IS_RECORD_MODE);
+		LOGGER.info("TARGET URL PREFIX " + urlPrefix + " record mode ? " + recordMode);
 	}
 
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		final String path = req.getRequestURI() + "?"
-				+ (req.getQueryString() == null ? "" : req.getQueryString());
-		final String url = URL_PREFIX + path;
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		final String path = req.getRequestURI() + "?" + (req.getQueryString() == null ? "" : req.getQueryString());
+		final String url = urlPrefix + path;
 		Response response = null;
 
-		boolean record = req.getParameter("mock_mode") != null ? "record"
-				.equalsIgnoreCase(req.getParameter("mock_mode"))
-				: IS_RECORD_MODE;
+		boolean record = req.getParameter("mock_mode") != null
+				? "record".equalsIgnoreCase(req.getParameter("mock_mode")) : recordMode;
 
 		if (record) {
 			LOGGER.info("GET RECORDING " + url + "\n");
@@ -134,8 +130,7 @@ public class Server extends HttpServlet {
 			}
 		} else {
 			response = (Response) FileUtils.readObject(url);
-			LOGGER.info("GET PLAYING " + url + " found " + (response != null)
-					+ "\n");
+			LOGGER.info("GET PLAYING " + url + " found " + (response != null) + "\n");
 
 		}
 		if (response == null) {
@@ -149,15 +144,12 @@ public class Server extends HttpServlet {
 	}
 
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		final String path = req.getRequestURI() + "?"
-				+ (req.getQueryString() == null ? "" : req.getQueryString());
-		final String url = URL_PREFIX + path;
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		final String path = req.getRequestURI() + "?" + (req.getQueryString() == null ? "" : req.getQueryString());
+		final String url = urlPrefix + path;
 		Response response = null;
 		@SuppressWarnings("unchecked")
-		Map<java.lang.String, java.lang.String[]> params = req
-				.getParameterMap();
+		Map<java.lang.String, java.lang.String[]> params = req.getParameterMap();
 		String body = new String(FileUtils.read(req.getInputStream()));
 
 		String key = null;
@@ -170,9 +162,8 @@ public class Server extends HttpServlet {
 				key = url + FileUtils.hash(body);
 			}
 		}
-		boolean record = req.getParameter("mock_mode") != null ? "record"
-				.equalsIgnoreCase(req.getParameter("mock_mode"))
-				: IS_RECORD_MODE;
+		boolean record = req.getParameter("mock_mode") != null
+				? "record".equalsIgnoreCase(req.getParameter("mock_mode")) : recordMode;
 
 		if (record) {
 			LOGGER.info("POST RECORDING " + key + ", body " + body + "\n");
@@ -180,16 +171,14 @@ public class Server extends HttpServlet {
 			if (params != null && params.size() > 0) {
 				response = post(url, params, toHeaders(req));
 			} else {
-				response = post(url, new String(req.getHeader("Content-Type")
-						.getBytes()), body, toHeaders(req));
+				response = post(url, new String(req.getHeader("Content-Type").getBytes()), body, toHeaders(req));
 			}
 			if (response.responseCode == 200) {
 				FileUtils.writeObject(response, key);
 			}
 		} else {
 			response = (Response) FileUtils.readObject(key);
-			LOGGER.info("POST PLAYING " + key + ", body " + body + ", found "
-					+ (response != null) + "\n");
+			LOGGER.info("POST PLAYING " + key + ", body " + body + ", found " + (response != null) + "\n");
 		}
 
 		//
@@ -200,37 +189,30 @@ public class Server extends HttpServlet {
 		res.setContentType(response.contentType);
 		OutputStream out = res.getOutputStream();
 		out.write(response.payload);
-		LOGGER.debug("POST SENDING " + key + ", response "
-				+ new String(response.payload));
+		LOGGER.debug("POST SENDING " + key + ", response " + new String(response.payload));
 
 		out.flush();
 	}
 
-	private Response get(final String u,
-			final Map<java.lang.String, java.lang.String> headers)
-			throws IOException {
+	private Response get(final String u, final Map<java.lang.String, java.lang.String> headers) throws IOException {
 		Response response = execute(u, new GetMethod(u), headers);
 		FileUtils.writeObject(response, u);
 		return response;
 	}
 
-	private Response post(final String u, final String contentType,
-			final String content,
-			final Map<java.lang.String, java.lang.String> headers)
-			throws IOException {
+	private Response post(final String u, final String contentType, final String content,
+			final Map<java.lang.String, java.lang.String> headers) throws IOException {
 
 		final PostMethod post = new PostMethod(u);
 		try {
-			post.setRequestEntity(new StringRequestEntity(content, contentType,
-					"UTF-8"));
+			post.setRequestEntity(new StringRequestEntity(content, contentType, "UTF-8"));
 		} catch (UnsupportedEncodingException e) {
 			throw new IOException("Failed to encode " + u, e);
 		}
 		return execute(u, post, headers);
 	}
 
-	private static final String toKey(
-			final Map<java.lang.String, java.lang.String[]> params) {
+	private static final String toKey(final Map<java.lang.String, java.lang.String[]> params) {
 		StringBuilder sb = new StringBuilder();
 		for (Map.Entry<String, String[]> e : params.entrySet()) {
 			for (String v : e.getValue()) {
@@ -242,10 +224,8 @@ public class Server extends HttpServlet {
 		return FileUtils.hash(sb.toString());
 	}
 
-	private Response post(final String u,
-			final Map<java.lang.String, java.lang.String[]> params,
-			final Map<java.lang.String, java.lang.String> headers)
-			throws IOException {
+	private Response post(final String u, final Map<java.lang.String, java.lang.String[]> params,
+			final Map<java.lang.String, java.lang.String> headers) throws IOException {
 
 		final PostMethod post = new PostMethod(u);
 		for (Map.Entry<String, String> h : headers.entrySet()) {
@@ -260,13 +240,11 @@ public class Server extends HttpServlet {
 		}
 		final int sc = httpClient.executeMethod(post);
 		String type = post.getResponseHeader("Content-Type").getValue();
-		return new Response(sc, FileUtils.read(post.getResponseBodyAsStream()),
-				type, toHeaders(post));
+		return new Response(sc, FileUtils.read(post.getResponseBodyAsStream()), type, toHeaders(post));
 
 	}
 
-	private Map<String, String[]> toHeaders(final HttpMethodBase post)
-			throws HttpException {
+	private Map<String, String[]> toHeaders(final HttpMethodBase post) throws HttpException {
 		Map<String, String[]> headers = new HashMap<String, String[]>();
 		for (Header h : post.getRequestHeaders()) {
 			@SuppressWarnings("deprecation")
@@ -292,16 +270,15 @@ public class Server extends HttpServlet {
 		return headers;
 	}
 
-	private Response execute(final String url, final HttpMethodBase method,
-			Map<String, String> headers) throws IOException {
+	private Response execute(final String url, final HttpMethodBase method, Map<String, String> headers)
+			throws IOException {
 		try {
 			for (Map.Entry<String, String> h : headers.entrySet()) {
 				method.setRequestHeader(h.getKey(), h.getValue());
 			}
 			final int sc = httpClient.executeMethod(method);
 			if (sc < 200 || sc > 299) {
-				throw new IOException("Unexpected status code: " + sc + ": "
-						+ method.getStatusText() + " -- " + url);
+				throw new IOException("Unexpected status code: " + sc + ": " + method.getStatusText() + " -- " + url);
 			}
 			final InputStream in = method.getResponseBodyAsStream();
 			String type = method.getResponseHeader("Content-Type").getValue();
